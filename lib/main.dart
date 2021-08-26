e751e1cb1bf300ee7f394cac7cbbfd68e2b75585
Deepraj Swamy <deeswasut1390@gmail.com>
2021-08-26 16:53:06 +0530

    Day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:r_e_commerce/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HOMEPAGE(),
+      // home: HOMEPAGE(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // primaryTextTheme: GoogleFonts.lektonTextTheme(),
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HOMEPAGE(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
